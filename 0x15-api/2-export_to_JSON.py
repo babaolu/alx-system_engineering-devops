1654f445dffb33abab5e9af531d1c0e26c78b32d
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-27 01:35:06 +0100

    Dumping to json files

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Displays todo list of an employee """
+import json
+import requests
+import sys
+
+userId = sys.argv[1]
+r = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                 .format(userId))
+emp = r.json().get("username")
+r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                 .format(userId)).json()
+
+dictlist = [{"task": x.get("title"), "completed": x.get("completed"),
+            "username": emp} for x in r]
+with open("{}.json".format(userId), "w") as jsonfile:
+    json.dump({userId: dictlist}, jsonfile)
