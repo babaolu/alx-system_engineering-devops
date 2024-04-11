8e8b22a04e840c633d73fe2176f8d9b2545955a7
Babatunde Adeoti <babaolu98@gmail.com>
2024-04-11 08:55:07 +0100

    Random debugging

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,14 @@ def top_ten(subreddit):
     """ Prints title of first 10 hot posts for subreddit """
     if not isinstance(subreddit, str):
         print(None)
+    url = 'https://www.reddit.com/r/'
+    headers = {'User-Agent': __name__}
+    cparams = {'limit': 10}
     t_list = []
     try:
-        r = requests.get('https://www.reddit.com/r/' + subreddit +
-                         '/hot.json?limit=10',
-                         headers={'User-Agent': '0-subs'},
+        r = requests.get('{}{}/hot.json'.format(url, subreddit),
+                         headers=headers,
+                         params=cparams,
                          allow_redirects=False)
         r.raise_for_status()
         t_list = r.json().get('data').get('children')
