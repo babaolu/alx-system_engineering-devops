d6b70ba17fdf0f1c23be58f6507071cc72e088eb
Babatunde Adeoti <babaolu98@gmail.com>
2024-04-11 08:22:26 +0100

    Using keyword variables

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,29 +5,29 @@ Implements function for printing first 10 hot posts for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """ Prints title of first 10 hot posts for subreddit """
     if not isinstance(subreddit, str):
         return hot_list
     url = 'https://www.reddit.com/r/'
+    headers = {'User-Agent': __name__}
+    cparams = {'limit': 100}
+    if after:
+        cparams['after'] = after
     t_list = []
     try:
-        if url in subreddit:
-            r = requests.get(subreddit, headers={'User-Agent': '0-subs'},
-                             allow_redirects=False)
-        else:
-            subreddit = url + subreddit + '/hot.json?limit=1'
-            r = requests.get(subreddit, headers={'User-Agent': '0-subs'},
-                             allow_redirects=False)
+        r = requests.get('{}{}/hot.json'.format(url, subreddit),
+                         headers=headers,
+                         params=cparams,
+                         allow_redirects=False)
         r.raise_for_status()
         t_list = r.json().get('data').get('children')
+        after = r.json().get('data').get('after')
     except requests.exceptions.RequestException as e:
         pass
 
     if t_list:
-        hot_list.append(t_list[0].get('data').get('title'))
-        subreddit = subreddit[:subreddit.find('?')]
-        post_id = t_list[0].get('kind') + '_' + t_list[0].get('data').get('id')
-        subreddit = subreddit + "?limit=1&after=" + post_id
-        recurse(subreddit, hot_list)
+        hot_list.extend([i.get('data').get('title') for i in t_list
+                        if not i.get('data').get('stickied')])
+        recurse(subreddit, hot_list, after)
     return hot_list
