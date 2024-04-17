e5633403350fd375c16d476100a3397bd044a8a5
Babatunde Adeoti <babaolu98@gmail.com>
2024-04-17 16:14:27 +0100

    Serving from RESTful

--- 0x1A-application_server/hbnb_flask_setup.sh
+++ 0x1A-application_server/hbnb_flask_setup.sh
@@ -2,7 +2,17 @@
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y net-tools python3 python3-dev python3-pip
-pip3 install Flask gunicorn
-sudo ufw allow in 5000
+
+sudo apt install -y net-tools build-essential pkg-config python3 python3-dev python3-pip default-libmysqlclient-dev zliblg-dev
+
+pip3 install Flask gunicorn SQLAlchemy
+
+MYSQLCLIENT_CFLAGS="-I/usr/include/mysql" MYSQLCLIENT_LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lmysqlclient" pip3 install mysqlclient
+
+sudo ufw allow in 5000/tcp
+sudo ufw allow in 5001/tcp
+sudo ufw allow in 5002/tcp
+
 sudo ufw --force reload
+git clone https://github.com/babaolu/AirBnB_clone_v2.git
+git clone https://github.com/babaolu/AirBnB_clone_v3.git
